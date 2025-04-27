d8454365faa1b514c64a9014ac17378622897aeb
Hvlikk <emiloszhalicki@gmail.com>
2025-04-27 20:04:26 +0200

    Added not-found component and adjusted routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ProductListComponentComponent } from './components/product-list-component/product-list-component.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent },
     {path: 'products', component: ProductListComponentComponent},
     {path: 'product/:id', component: ProductDetailComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: '**', redirectTo: '/home'}
+    {path: 'not-found', component: NotFoundComponent},
+    {path: '**', redirectTo: '/not-found'}
 ];
